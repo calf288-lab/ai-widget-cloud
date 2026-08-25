7b41973b45e9cd77bd3113089032eab5d15eeebb
germes <calf288@gmail.com>
2026-08-25 18:11:56 +0300

    Add CORS support for POST and OPTIONS methods

--- api/chat.js
+++ api/chat.js
@@ -7,6 +7,7 @@ const CLIENTS = {
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
   const { domain, text } = req.body || {};
   const cfg = CLIENTS[domain];
